129bc152fd95261bf06946bef9e2f80275f9041d
hboudar <boudarhamza12@gmail.com>
2026-04-19 23:36:19 +0100

    adding examples

--- backend-growth-2026/week-01/Remove-Element/Solution.js
+++ backend-growth-2026/week-01/Remove-Element/Solution.js
@@ -1,8 +1,5 @@
-/**
- * @param {number[]} nums
- * @param {number} val
- * @return {number}
- */
+/*My first Approach*/
+
 var removeElement = function(nums, val) {
     let k = 0;
     let j = nums.length - 1;
@@ -14,8 +11,28 @@ var removeElement = function(nums, val) {
                             nums[k] -= nums[j],
                             j--)
                         : k++;
-        console.log(nums);
     }
 
     return k;
 };
+
+
+// Example usage:
+
+let nums = [3, 2, 2, 3];
+let val = 3;
+
+console.log(removeElement(nums, val)); // Output: 2
+console.log(nums); // Output: [2, 2, _, _]
+
+nums = [0,1,2,2,3,0,4,2];
+val = 2;
+
+console.log(removeElement(nums, val)); // Output: 5
+console.log(nums); // Output: [0,1,3,0,4,_,_,_]
+
+nums = [1];
+val = 1;
+
+console.log(removeElement(nums, val)); // Output: 0
+console.log(nums); // Output: [_]
